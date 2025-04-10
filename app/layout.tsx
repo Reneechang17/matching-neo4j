a85bce05e2653dc02cc672b47158546fb99ec7b7
Reneechang17 <minnie90061717@gmail.com>
2025-04-10 00:24:26 -0400

    connect neo4j and create users

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,20 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+const inter = Inter({
+  variable: "--font-sans",
   subsets: ["latin"],
 });
 
@@ -25,7 +31,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          inter.variable,
+        )}
       >
         {children}
       </body>
---
+++ app/neo4j.action.ts
@@ -0,0 +1,23 @@
+"use server";
+
+import { driver } from "@/db";
+import { Neo4JUser } from "@/types";
+
+export const getUserByID = async (id: string) => {
+  const result = await driver.executeQuery(
+    `MATCH (u: User {applicationId: $applicationId}) RETURN u`,
+    { applicationId: id }
+  );
+
+  const users = result.records.map((record) => record.get("u").properties);
+  if (users.length === 0) return null;
+  return users[0] as Neo4JUser;
+};
+
+export const createUser = async (user: Neo4JUser) => {
+    const { applicationId, email, firstname, lastname } = user;
+    await driver.executeQuery(
+        `CREATE (u: User {applicationId: $applicationId, email: $email, firstname: $firstname, lastname: $lastname})`,
+        { applicationId, email, firstname, lastname }
+    )
+}
